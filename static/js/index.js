ba9e5bef235ef5b244d729a424bc889e24e82f6f
krisdb2009 <dylan@belowaverage.org>
2017-01-11 17:11:12 -0500

    Broken

--- static/js/index.js
+++ static/js/index.js
@@ -1,20 +1,43 @@
+var mem = {};
+function loadTab(tid, pageObj) {
+    $.each(pageObj, function(k) {
+        if(k == 'html') {
+            $('#tabs .tab[tid='+tid+']').append(pageObj[k]);
+        } else if(k == 'css') {
+            $('#tabs .tab[tid='+tid+']').append('<style>'+pageObj[k]+'</style>');
+        } else if(k == 'javascript') {
+            $('#tabs .tab[tid='+tid+']').append('<script>'+pageObj[k]+'</script>');
+        } else {
+            //error
+        }
+    });
+}
 function changeTab(tid) {
     if(tid == '') {
-        tid = 'home';
+        tid = 'Home';
     }
     tid = tid.replace('#', '');
+    $('#tabs div').html('');
+    if(typeof mem[tid] !== 'object') {
+        $.get('config/pages/'+tid+'.html', function(data) {
+            mem[tid] = data;
+            loadTab(tid, data);
+        });
+    } else {
+        loadTab(tid, mem[tid]);
+    }
     $('#header .links a, #tabs div').removeClass('active');
     $('#header .links a[tid='+tid+'], #tabs div[tid='+tid+']').addClass('active');
 }
 $(document).ready(function() {
+    $.getJSON('config/main.json', function(data) {
+        $.each(data.pages, function() {
+            $('#header .links').append('<a href="#'+this+'" tid="'+this+'">'+this+'</a>');
+            $('#tabs').append('<div class="tab" tid="'+this+'"></div>');
+        });
+    });
     changeTab(window.location.hash);
     window.onhashchange = function() {
         changeTab(window.location.hash);
     };
-    $('#sb').click(function() {
-        $('#homePrev').addClass('swanimate');
-        setTimeout(function() {
-            window.location = 'https://belowaverage.org/webdows/';
-        }, 2500);
-    });
 });
