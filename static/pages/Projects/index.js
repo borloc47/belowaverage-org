b058e8cebb7f43bb82a716fcd4d6bac6740a0cd6
krisdb2009 <dylan@belowaverage.org>
2017-05-13 11:19:38 -0400

    Update

--- static/pages/Projects/index.js
+++ static/pages/Projects/index.js
@@ -1,1 +1,13 @@
-$.getJSON('config/pages/Projects.json')
+$.getJSON('config/pages/Projects.json', function(data) {
+	$.each(data, function(k) {
+		var html = '<span class="proj"><img src="'+this.background+'"><div class="head"><div class="ttl">'+k+'</div><div class="slogan">'+this.slogan+'</div></div><div class="links">';
+		$.each(this.links, function(k) {
+			if(typeof this.target == 'undefined') {
+				this.target = '';
+			}
+			html = html + '<a class="'+this.class+'" target="'+this.target+'" href="'+this.href+'">'+k+'</a>';
+		});
+		html = html + '</div></span>';
+		$('#projs').append(html);
+	});
+});
