dbc33ffdbf02678a71ccd41f900db6cac2860461
krisdb2009 <dylan@belowaverage.org>
2017-06-15 02:10:28 -0400

    Wow

--- static/pages/Projects/index.js
+++ static/pages/Projects/index.js
@@ -1,13 +1,36 @@
-$.getJSON('config/pages/Projects.json', function(data) {
+function draw(data) {
 	$.each(data, function(k) {
-		var html = '<span class="proj"><img src="'+this.background+'"><div class="head"><div class="ttl">'+k+'</div><div class="slogan">'+this.slogan+'</div></div><div class="links">';
+		var exp = '';
+		var body = '';
+		if(typeof mem.Projects.hash.more == 'string' && mem.Projects.hash.more == k) {
+			exp = 'expanded';
+			body = this.expand;
+		}
+		var html = '<span project="'+k+'" class="proj '+exp+'"><img src="'+this.background+'"><div class="head"><div class="ttl">'+k+'</div><div class="slogan">'+this.slogan+'</div>';
+		html = html + body;
+		html = html + '</div><div class="links">';
 		$.each(this.links, function(k) {
 			if(typeof this.target == 'undefined') {
 				this.target = '';
 			}
 			html = html + '<a class="'+this.class+'" target="'+this.target+'" href="'+this.href+'">'+k+'</a>';
 		});
+		if(typeof this.expand == 'string') {
+			if(typeof mem.Projects.hash.more == 'string') {
+				html = html + '<a href="#Projects">Less</a>';
+			} else {
+				html = html + '<a href="#Projects,more.'+k+'">More</a>';
+			}
+		}
 		html = html + '</div></span>';
 		$('#projs').append(html);
 	});
-});
+}
+if(typeof mem.Projects.config == 'object') {
+	draw(mem.Projects.config);
+} else {
+	$.getJSON('config/pages/Projects.json', function(data) {
+		mem.Projects.config = data;
+		draw(data);
+	});
+}
